5c4c24ba106f77295c5eb276758c0e9f9c3d5f51
Akarshgu <akarshgupta705415@gmail.com>
2023-03-10 18:45:40 +0530

    updated

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express=require("express");
 const bollywood=require("./router/bollywood")
 const hollywood=require("./router/hollywood")
-const dotenv=require("dotenv") //npm i dotenv
 const app=express();
-dotenv.config();
-// const scedf=
 app.use("/api",bollywood)
 app.use("/api",hollywood)
 app.listen ( process.env.PORT,()=>{
